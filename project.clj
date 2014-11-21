f332a7af7e8fd1f98d6214df71de66ea60b96bba
Paul Madden <maddenp@colorado.edu>
2014-11-21 12:26:43 -0700

    update project.clj

--- project.clj
+++ project.clj
@@ -1,9 +1,10 @@
-(defproject revdiff "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+(defproject revdiff "1.0"
+;; :aot [revdiff.core]
   :dependencies [[org.clojure/clojure "1.6.0"]]
+  :description "A Subversion history search / diff tool"
+  :license {:name "Apache License Version 2.0" :url "http://www.apache.org/licenses/LICENSE-2.0.html"}
   :main ^:skip-aot revdiff.core
+  :profiles {:uberjar {:aot :all}}
   :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+  :uberjar-name "revdiff.jar"
+  :url "https://github.com/maddenp/revdiff")
